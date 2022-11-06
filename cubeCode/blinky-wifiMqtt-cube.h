10a4e56d11114009bb343223732510fe290f9ec2
David McGinnis <david.mcginnis@bl-mc.se>
2022-11-06 13:19:50 +0100

    wifi cred change working

--- cubeCode/blinky-wifiMqtt-cube.h
+++ cubeCode/blinky-wifiMqtt-cube.h
@@ -1,18 +1,25 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-WiFiClient g_wifiClient;
-PubSubClient g_mqttClient(g_wifiClient);
+WiFiClient    g_wifiClient;
+PubSubClient  g_mqttClient(g_wifiClient);
+WiFiServer    g_wifiServer(80);
 unsigned long g_lastMsgTime = 0;
-int g_wifiStatus = 0;
-unsigned long g_wifiTimeout = 15000;
+unsigned long g_lastWirelessBlink;
+int           g_wifiStatus = 0;
+unsigned long g_wifiTimeout = 25000;
 unsigned long g_wifiRetry = 30000;
 unsigned long g_wifiLastTry = 0;
-int g_publishInterval  = 2000;
-boolean g_publishOnInterval = true;
-boolean g_publishNow = false;
-int g_commLEDPin = LED_BUILTIN;
-boolean g_commLEDState = false;
+int           g_publishInterval  = 2000;
+boolean       g_publishOnInterval = true;
+boolean       g_publishNow = false;
+int           g_commLEDPin = LED_BUILTIN;
+boolean       g_commLEDState = false;
+int           g_resetButtonPin = -1;
+boolean       g_wifiAccessPointMode = false;
+boolean       g_webPageServed = false;
+boolean       g_webPageRead = false;
+volatile unsigned long g_resetButtonDownTime = 0;
 
 union SubscribeData
 {
@@ -27,17 +34,169 @@ union SubscribeData
 
 
 
+void setupWifiAp()
+{
+  g_wifiServer = WiFiServer(80);
+  Serial.println("Reset");
+  WiFi.disconnect();
+  g_wifiAccessPointMode = true;
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("picoTest");
+  Serial.print("[+] AP Created with IP Gateway ");
+  Serial.println(WiFi.softAPIP());
+  g_wifiServer.begin();
+  Serial.print("Connected to wifi. My address:");
+  IPAddress myAddress = WiFi.localIP();
+  Serial.println(myAddress);
+  g_webPageServed = false;
+  g_webPageRead = false;
+}
+void serveWebPage()
+{
+  if (g_webPageServed) return;
+  WiFiClient client = g_wifiServer.available();
+
+  if (client) 
+  {
+    Serial.println("new client");
+    String header = "";
+    while (client.connected() && !g_webPageServed) 
+    {
+      if (client.available()) 
+      {
+        char c = client.read();
+        header.concat(c);
+      }
+      else
+      {
+        break;
+      }
+    }
+    Serial.println(header);
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: text/html");
+    client.println("Connection: close");  // the connection will be closed after completion of the response
+    client.println();
+    client.println("<!DOCTYPE HTML>");
+    client.println("<html>");
+    client.println("<div>");
+    client.println("<form action=\"/disconnected\" method=\"POST\">");
+    client.println("<table align=\"center\">");
+    client.println("<tr>");
+    client.println("<td><label for=\"ssid\">SSID</label></td>");
+    client.println("<td><input name=\"ssid\" id=\"ssid\" type=\"text\"/></td>");
+    client.println("</tr>");
+    client.println("<tr>");
+    client.println("<td><label for=\"password\">Password</label></td>");
+    client.println("<td><input name=\"password\" id=\"password\" type=\"password\"/></td>");
+    client.println("</tr>");
+    client.println("<tr>");
+    client.println("<td></td>");
+    client.println("<td><input type=\"submit\"/></td>");
+    client.println("</tr>");
+    client.println("</table>");
+    client.println("</form>");
+    client.println("</div>");
+    client.println("</html>");
+    g_webPageServed = true;
+    delay(1);
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
+void readWebPage()
+{
+  if (!g_webPageServed) return;
+  if (g_webPageRead) return;
+  WiFiClient client = g_wifiServer.available();
+
+  if (client) 
+  {
+    String header = "";
+    while (client.connected() && !g_webPageRead) 
+    {
+      if (client.available()) 
+      {
+        char c = client.read();
+        header.concat(c);
+      }
+      else
+      {
+        break;
+      }
+    }
+    if (header.indexOf("POST") >= 0)
+    {
+      Serial.println(header);
+      String data = header.substring(header.indexOf("ssid="),header.length());
+      g_ssid = data.substring(5,data.indexOf("&password="));
+      g_wifiPassword = data.substring(data.indexOf("&password=") + 10,data.length());
+      Serial.println(header);
+      Serial.println(data);
+      Serial.print("X");
+      Serial.print(g_ssid);
+      Serial.print("X");
+      Serial.print(g_wifiPassword);
+      Serial.println("X");
+      client.println("HTTP/1.1 200 OK");
+      client.println("Content-Type: text/html");
+      client.println("Connection: close");  // the connection will be closed after completion of the response
+      client.println();
+      client.println("<!DOCTYPE HTML>");
+      client.println("<html>");
+      client.println("<div>");
+      client.println("</div>");
+      client.println("</html>");
+      g_webPageRead = true;
+    }
+    delay(1);
+    client.stop();
+    if (g_webPageRead)
+    {
+      g_wifiAccessPointMode = false;
+      WiFi.disconnect();
+    }
+  }
+}
+void resetButtonHandler()
+{
+  if (digitalRead(g_resetButtonPin)) 
+  {
+    Serial.println("Button Down");
+    g_resetButtonDownTime = millis();
+  }
+  else
+  {
+    Serial.println("Button up");
+    if ((millis() - g_resetButtonDownTime) > 10000)
+    {
+      setupWifiAp();
+    }
+  }
+}
+
 void setup_wifi() 
 {
   if ((millis() - g_wifiLastTry) < g_wifiRetry) return;
+  g_commLEDState = 1;
+  digitalWrite(g_commLEDPin, g_commLEDState);
   delay(10);
   g_wifiLastTry = millis();
   Serial.println();
   Serial.print("Connecting to ");
-  Serial.println(ssid);
+  Serial.println(g_ssid);
+  char ssidBuffer[40];
+  char pwBuffer[40];
+  g_ssid.toCharArray(ssidBuffer,40);
+  g_wifiPassword.toCharArray(pwBuffer,40);
+  
+  Serial.print("SSID: ");
+  Serial.println(ssidBuffer);
+  Serial.print("Wifi Password: ");
+  Serial.println(pwBuffer);
 
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, wifiPassword);
+  WiFi.begin(ssidBuffer, pwBuffer);
   
   while ((g_wifiStatus != WL_CONNECTED) && ((millis() - g_wifiLastTry) < g_wifiTimeout))
   {
@@ -112,7 +271,7 @@ void reconnect()
     }
   }
 }
-void initBlinkyBus(int publishInterval, boolean publishOnInterval, int commLEDPin)
+void initBlinkyBus(int publishInterval, boolean publishOnInterval, int commLEDPin, int resetButtonPin)
 {
   Serial.println("Starting Communications");
   g_publishInterval = publishInterval;
@@ -125,47 +284,77 @@ void initBlinkyBus(int publishInterval, boolean publishOnInterval, int commLEDPi
   g_commLEDPin = commLEDPin;
   pinMode(g_commLEDPin, OUTPUT);
   digitalWrite(g_commLEDPin, g_commLEDState);
+  g_resetButtonPin = resetButtonPin;
+  if (g_resetButtonPin > 0 )
+  { 
+    attachInterrupt(digitalPinToInterrupt(g_resetButtonPin), resetButtonHandler, CHANGE);
+    pinMode(g_resetButtonPin, INPUT);
+  }
+
   
 }
 void blinkyBusLoop()
 {
-  g_wifiStatus = WiFi.status();
-  if (g_wifiStatus == WL_CONNECTED)
+  if (g_wifiAccessPointMode)
   {
-    if (!g_mqttClient.connected()) 
-    {
-      reconnect();
-    }
-    g_mqttClient.loop();
-  
-    unsigned long now = millis();
-    if (g_publishOnInterval)
-    {
-      if (now - g_lastMsgTime > g_publishInterval) 
-      {
-        g_lastMsgTime = now;
-        g_commLEDState = !g_commLEDState;
-        digitalWrite(g_commLEDPin, g_commLEDState);
-        blinkyBus.watchdog = blinkyBus.watchdog + 1;
-        if (blinkyBus.watchdog > 32760) blinkyBus.watchdog = 32760 ;
-        g_mqttClient.publish(mqttPublishTopic, (uint8_t*)blinkyBus.buffer, BLINKYMQTTBUSBUFSIZE * 2);
-      }
-    }
-    if (g_publishNow)
-    {
-        g_lastMsgTime = now;
-        g_commLEDState = !g_commLEDState;
-        digitalWrite(g_commLEDPin, g_commLEDState);
-        g_publishNow = false;
-        blinkyBus.watchdog = blinkyBus.watchdog + 1;
-        if (blinkyBus.watchdog > 32760) blinkyBus.watchdog = 32760 ;
-        g_mqttClient.publish(mqttPublishTopic, (uint8_t*)blinkyBus.buffer, BLINKYMQTTBUSBUFSIZE * 2);
-    }
+    serveWebPage();
+    readWebPage();
   }
   else
   {
-    setup_wifi();
+    g_wifiStatus = WiFi.status();
+    unsigned long now = millis();
+    if (g_wifiStatus == WL_CONNECTED)
+    {
+      if (!g_mqttClient.connected()) 
+      {
+        reconnect();
+      }
+      g_mqttClient.loop();
+    
+      if (g_publishOnInterval)
+      {
+        if (g_commLEDState > 0)
+        {
+          if (now - g_lastMsgTime > 10) 
+          {
+            g_commLEDState = 0;
+            digitalWrite(g_commLEDPin, g_commLEDState);
+          }
+        }
+        if (now - g_lastMsgTime > g_publishInterval) 
+        {
+          g_lastMsgTime = now;
+          g_commLEDState = 1;
+          digitalWrite(g_commLEDPin, g_commLEDState);
+          blinkyBus.watchdog = blinkyBus.watchdog + 1;
+          if (blinkyBus.watchdog > 32760) blinkyBus.watchdog = 0 ;
+          g_mqttClient.publish(mqttPublishTopic, (uint8_t*)blinkyBus.buffer, BLINKYMQTTBUSBUFSIZE * 2);
+        }
+      }
+      if (g_publishNow)
+      {
+          g_lastMsgTime = now;
+          g_commLEDState = 1;
+          digitalWrite(g_commLEDPin, g_commLEDState);
+          g_publishNow = false;
+          blinkyBus.watchdog = blinkyBus.watchdog + 1;
+          if (blinkyBus.watchdog > 32760) blinkyBus.watchdog = 0 ;
+          g_mqttClient.publish(mqttPublishTopic, (uint8_t*)blinkyBus.buffer, BLINKYMQTTBUSBUFSIZE * 2);
+      }
+    }
+    else
+    {
+      setup_wifi();
+      if ((now - g_lastWirelessBlink) > 100)
+      {
+        g_commLEDState = !g_commLEDState;
+        g_lastWirelessBlink = now;
+        digitalWrite(g_commLEDPin, g_commLEDState);
+      }
+    }
   }
+
 }
 void publishBlinkyBusNow()
 {
--- cubeCode/cubeCode.ino
+++ cubeCode/cubeCode.ino
@@ -1,12 +1,5 @@
 
-const char* ssid = "xxxxx";
-const char* wifiPassword = "xxxxx";
-const char* mqttServer = "xxxxx";
-const char* mqttUsername = "xxxxx";
-const char* mqttPassword = "xxxxx";
-const char* mqttPublishTopic = "blinky-mqtt-led/XX/reading";
-const char* mqttSubscribeTopic = "blinky-mqtt-led/XX/setting";
-
+#include "wifiCredentials.h"
 
 #define BLINKYMQTTBUSBUFSIZE  5
 union BlinkyBusUnion
@@ -15,9 +8,9 @@ union BlinkyBusUnion
   {
     int16_t state;
     int16_t watchdog;
+    int16_t chipTemp;
     int16_t led1;
     int16_t led2;
-    int16_t led3;
   };
   int16_t buffer[BLINKYMQTTBUSBUFSIZE];
 } blinkyBus;
@@ -30,32 +23,31 @@ void subscribeCallback(uint8_t address, int16_t value)
 
 int led1Pin = 10;
 int led2Pin = 11;
-int led3Pin = 12;
 
 void setup() 
 {
   pinMode(led1Pin, OUTPUT);
   pinMode(led2Pin, OUTPUT);
-  pinMode(led3Pin, OUTPUT);
   Serial.begin(115200);
-  delay(5000);
-  initBlinkyBus(2000,true, LED_BUILTIN);
+  delay(1000);
+  initBlinkyBus(2000,true, LED_BUILTIN, 16);
 
   blinkyBus.led1 = 0;
   blinkyBus.led2 = 0;
-  blinkyBus.led3 = 0;
   setLeds();
+
+
 }
 
 void loop() 
 {
   blinkyBusLoop();
+  blinkyBus.chipTemp = (int16_t) (analogReadTemp() * 100.0);
 //  publishBlinkyBusNow(); 
 }
 
 void setLeds()
 {
-  digitalWrite(led1Pin, blinkyBus.led1);    
-  digitalWrite(led2Pin, blinkyBus.led2);    
-  digitalWrite(led3Pin, blinkyBus.led3);    
+  analogWrite(led1Pin, blinkyBus.led1);    
+  analogWrite(led2Pin, blinkyBus.led2);    
 }
